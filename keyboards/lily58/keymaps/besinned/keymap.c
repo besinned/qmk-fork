c432dcc23b31ca55cf05bc0d78b079410ff3feb0
Dennis Bykkov <besinned@gmail.com>
2021-07-11 15:32:54 -0700

    Added besinned lily58 keymap

--- keyboards/lily58/keymaps/besinned/keymap.c
+++ keyboards/lily58/keymaps/besinned/keymap.c
@@ -19,12 +19,14 @@ extern uint8_t is_master;
 #define _LOWER 1
 #define _RAISE 2
 #define _ADJUST 3
+#define _ARROW 4
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
   RAISE,
   ADJUST,
+  ARROW
 };
 
 
@@ -40,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |LCTRL |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |   \  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
+ *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| ARROW |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -50,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
   KC_LSFT, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LCTRL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_BSLASH, \
-                             KC_LALT, KC_LGUI,LOWER, KC_SPC,   KC_ENT,   RAISE,   KC_BSPC, KC_RGUI \
+                             KC_LALT, KC_LGUI,MO(_LOWER), KC_SPC,   KC_ENT,   MO(_RAISE),   KC_BSPC, TG(_ARROW) \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -115,6 +117,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD,\
                              _______, _______, _______, _______, _______,  _______, _______, _______ \
+  ),
+  /* ARROW
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |                    |      |      |  ^   |      |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |-------.    ,-------|      |  <   |  v   |  >   |      |      |
+ * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |DELETE| RGUI |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
+  [_ARROW] = LAYOUT( \
+  _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______,                   _______, _______, KC_UP, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______,                   _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, \
+                             _______, _______, _______, _______, _______,  _______, _______, _______ \
   )
 };
 
@@ -172,48 +195,49 @@ void oled_task_user(void) {
 #endif // OLED_DRIVER_ENABLE
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (record->event.pressed) {
+
 #ifdef OLED_DRIVER_ENABLE
-    // set_keylog(keycode, record);
-#endif
+  if (record->event.pressed) {
+    set_keylog(keycode, record);
     set_timelog();
   }
+#endif
 
-  switch (keycode) {
-    case QWERTY:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_QWERTY);
-      }
-      return false;
-      break;
-    case LOWER:
-      if (record->event.pressed) {
-        layer_on(_LOWER);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_LOWER);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case RAISE:
-      if (record->event.pressed) {
-        layer_on(_RAISE);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_RAISE);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case ADJUST:
-        if (record->event.pressed) {
-          layer_on(_ADJUST);
-        } else {
-          layer_off(_ADJUST);
-        }
-        return false;
-        break;
-  }
+  // switch (keycode) {
+  //   case QWERTY:
+  //     if (record->event.pressed) {
+  //       set_single_persistent_default_layer(_QWERTY);
+  //     }
+  //     return false;
+  //     break;
+  //   case LOWER:
+  //     if (record->event.pressed) {
+  //       layer_on(_LOWER);
+  //       update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+  //     } else {
+  //       layer_off(_LOWER);
+  //       update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+  //     }
+  //     return false;
+  //     break;
+  //   case RAISE:
+  //     if (record->event.pressed) {
+  //       layer_on(_RAISE);
+  //       update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+  //     } else {
+  //       layer_off(_RAISE);
+  //       update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+  //     }
+  //     return false;
+  //     break;
+  //   case ADJUST:
+  //       if (record->event.pressed) {
+  //         layer_on(_ADJUST);
+  //       } else {
+  //         layer_off(_ADJUST);
+  //       }
+  //       return false;
+  //       break;
+  // }
   return true;
 }
